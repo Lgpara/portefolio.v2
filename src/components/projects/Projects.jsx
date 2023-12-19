c126cbde93e599b0c4d9be75e424e49ccdb0f4fc
Louis Gerber <louisgerber004@gmail.com>
2023-12-19 21:58:56 +0100

    save

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,14 @@
+import projectData from "../../appdata/projects.json"
+
 export default function Projects(){
     return(
-        <div className="projectsContainer"></div>
+        <div className="projectsContainer">
+            {projectData.map((project)=>(
+                <div className="projectCard">
+                    <h3 className="projectTitle">{project.titre}</h3>
+                    <p className="projectDescription">{project.description}</p>
+                </div>
+            ))}
+        </div>
     )
 }
