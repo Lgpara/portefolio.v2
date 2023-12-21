d364a45c69d1e86e83477c5f57474087a26b08ad
Louis Gerber <louisgerber004@gmail.com>
2023-12-21 11:54:49 +0100

    version 0.0

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,59 +1,128 @@
 import { useState } from "react";
 import projectData from "../../appdata/projects.json";
+import img from "../../appdata/images.json"
 import "./projects.css";
 
 export default function Projects() {
-  const [projectsImgSelect, setProjectsImgSelect] = useState(projectData.map(()=>(0)))
-  const [projectsLength, setProjectsLength] = useState(projectData.map((project)=>(project.images.length)))
-  console.log(projectsImgSelect)
-  function handleChevronClick(projectIndex, way){
-    const newArray = [...projectsImgSelect]
-    if(way === "right"){
-      const currentIndex = projectsImgSelect[projectIndex]
-      let newIndex = currentIndex + 1
-      if (newIndex > projectsLength[projectIndex] - 1){
-        newIndex = 0
-        newArray[projectIndex]= newIndex
-        setProjectsImgSelect(newArray)
+  const [projectsImgSelect, setProjectsImgSelect] = useState(
+    projectData.map(() => 0)
+  );
+  const [projectsLength, setProjectsLength] = useState(
+    projectData.map((project) => project.images.length)
+  );
+  console.log(projectsImgSelect);
+  function handleChevronClick(projectIndex, way) {
+    const newArray = [...projectsImgSelect];
+    if (way === "right") {
+      const currentIndex = projectsImgSelect[projectIndex];
+      let newIndex = currentIndex + 1;
+      if (newIndex > projectsLength[projectIndex] - 1) {
+        newIndex = 0;
+        newArray[projectIndex] = newIndex;
+        setProjectsImgSelect(newArray);
       } else {
-        newArray[projectIndex]= newIndex
-        setProjectsImgSelect(newArray)
+        newArray[projectIndex] = newIndex;
+        setProjectsImgSelect(newArray);
       }
-    } else if(way ==="left"){
-      const currentIndex = projectsImgSelect[projectIndex]
-      let newIndex = currentIndex - 1
-      if (newIndex < 0){
-        newIndex = projectsLength[projectIndex] - 1
-        newArray[projectIndex]= newIndex
-        setProjectsImgSelect(newArray)
+    } else if (way === "left") {
+      const currentIndex = projectsImgSelect[projectIndex];
+      let newIndex = currentIndex - 1;
+      if (newIndex < 0) {
+        newIndex = projectsLength[projectIndex] - 1;
+        newArray[projectIndex] = newIndex;
+        setProjectsImgSelect(newArray);
       } else {
-        newArray[projectIndex]= newIndex
-        setProjectsImgSelect(newArray)
+        newArray[projectIndex] = newIndex;
+        setProjectsImgSelect(newArray);
       }
-    }else {
-      return
+    } else {
+      return;
     }
   }
+
+  const getIconSource = (name) => {
+    const stacks = img.icons.stacks
+    const tools = img.icons.tools
+    const techs = img.icons.techs
+    for (const stack of stacks) {
+      if (stack.name === name) {
+        return stack.source;
+      }
+    }
+  
+    for (const tool of tools) {
+      if (tool.name === name) {
+        return tool.source;
+      }
+    }
+  
+    for (const tech of techs) {
+      if (tech.name === name) {
+        return tech.source;
+      }
+    }
+  
+    return null;
+  }
+
   return (
     <div className="projectsContainer">
       <h2>My work</h2>
       <div className="projectCardsContainer">
         {projectData.map((project, index) => (
           <>
-            <div className={index % 2 === 0 ? "projectCard" : "projectCard rowReverse"}>
+            <div
+              className={
+                index % 2 === 0 ? "projectCard" : "projectCard rowReverse"
+              }
+            >
               <div className="carouselImg">
-                <img src={project.images[projectsImgSelect[index]]} alt="" className="projectImage" />
+                <img
+                  src={project.images[projectsImgSelect[index]]}
+                  alt=""
+                  className="projectImage"
+                  transition="opacity 0.5s ease"
+                />
                 <div className="arrowContainer">
-                  <i class="fa-solid fa-circle-chevron-left" onClick={()=>handleChevronClick(index, "left")}></i>
-                  <i class="fa-solid fa-circle-chevron-right" onClick={()=>handleChevronClick(index, "right")}></i>
+                  <i
+                    class="fa-solid fa-circle-chevron-left"
+                    onClick={() => handleChevronClick(index, "left")}
+                  ></i>
+                  <i
+                    class="fa-solid fa-circle-chevron-right"
+                    onClick={() => handleChevronClick(index, "right")}
+                  ></i>
                 </div>
               </div>
               <div className="infos">
                 <h3 className="projectTitle">{project.titre}</h3>
-                <div className="projectStacks">{project.stacks}</div>
+
+
+                <div className="projectStacks">{project.stacks.map((stack)=>(
+                  <img className="projectStacksIcon"  src={getIconSource(stack)} />
+                ))}</div>
+
+
+                <p>{project.description}</p>
+                <div className="linksContainer">
+                  {project.link.website !== "" ? (
+                    <a href={project.link.website} className="website">
+                      <i class="fa-solid fa-globe"></i>
+                      <p>Visit website</p>
+                    </a>
+                  ) : null}
+                  {project.link.github !== "" ? (
+                    <a href={project.link.github} className="gh">
+                      <i class="fa-brands fa-github"></i>
+                      <p>GitHub Code</p>
+                    </a>
+                  ) : null}
+                </div>
               </div>
             </div>
-            {index < projectData.length - 1 ?<div className="separationBar"></div> : null}
+            {index < projectData.length - 1 ? (
+              <div className="separationBar"></div>
+            ) : null}
           </>
         ))}
       </div>
@@ -61,8 +130,3 @@ export default function Projects() {
   );
 }
 
-{/* <div className="links">
-  <i class="fa-solid fa-globe"></i>
-  <i class="fa-brands fa-github"></i>
-  <i class="fa-solid fa-info"></i>
-</div> */}
